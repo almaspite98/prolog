f7e599ffd6437c693bee06f369883a0d9a28fd56
dbobula <bobula.dalma@grape.hu>
2021-05-12 09:05:02 +0200

    clean up 01

--- nhf-keret/sudoku.pl
+++ nhf-keret/sudoku.pl
@@ -1,8 +1,3 @@
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   Sudoku CLP(Z) formulation.
-   Written Feb. 2008 by Markus Triska  (triska@metalevel.at)
-   Public domain code.
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 -module(sudoku).
 -export([ sudoku/2]).
 :- use_module(library(clpfd)), use_module(library(lists)).
@@ -17,45 +12,33 @@ apply_dist(M,N,D,I,0,C):-
     L0 is L - 1,
     I == L0,
     nth0_mtx(M,I,0,E1),
-%    E2 is E1 + N,
-%    E3 is E1 + N,
     nth0_mtx(D,I,0,D1),
-
     dist(D1,E1,E1,E1,N),
-
     length(M, K),
     C1 is C + 1,
     I1 is truncate(C1/K),
     J1 is C1 mod K,
     apply_dist(M,N,D,I1,J1,C1).
 apply_dist(M,N,D,I,J,C):-
-
     length(M,L),
     L0 is L - 1,
     I == L0,
     nth0_mtx(M,I,J,E1),
-%    E3 is E1 + N,
     J0 is J-1,
     nth0_mtx(M,I,J0,E3),
     nth0_mtx(D,I,J,D1),
-
     dist(D1,E1,E1,E3,N),
-
     length(M, K),
     C1 is C + 1,
     I1 is truncate(C1/K),
     J1 is C1 mod K,
     apply_dist(M,N,D,I1,J1,C1).
 apply_dist(M,N,D,I,0,C):-
-
     nth0_mtx(M,I,0,E1),
-%    E3 is E1 + N,
     I0 is I + 1,
     nth0_mtx(M,I0,0,E2),
     nth0_mtx(D,I,0,D1),
     dist(D1,E1,E2,E1,N),
-
-
     length(M, K),
     C1 is C + 1,
     I1 is truncate(C1/K),
@@ -74,8 +57,6 @@ nth0_mtx(M,I,J,E1),
     nth0_mtx(M,I,J0,E3),
     nth0_mtx(D,I,J,D1),
     dist(D1,E1,E2,E3,N),
-
-%
     length(M, K),
     C1 is C + 1,
     I1 is truncate(C1/K),
@@ -88,9 +69,6 @@ nth0_mtx(M,I,J,E):-
     nth0(I,M,L),
     nth0(J,L,E).
 
-%dist_contr(E1,E2,N):-
-%    abs(E1-E2) #= N.
-%
 
 dist([],E1,E2,E3,N):-
     abs(E1-E2) #\= N,
@@ -127,35 +105,23 @@ dist([A,s,w],E1,E2,E3,N):-
 
 
 sudoku(s(N,T), Rows) :-
-    print(T),
     length(T, L),
     length(Rows, L), maplist(same_length(Rows), Rows),
     append(Rows, Vs),
     domain(Vs, 1, L),
-%        Vs in 1..9,
     maplist(all_distinct, Rows),
     transpose(Rows, Columns),
     maplist(all_distinct, Columns),
     cella(Rows, 1),
     apply_dist(Rows,N,T,0,0,0),
-    maplist(labeling([ff]), Rows),
-    maplist(portray_clause, Rows).
+    maplist(labeling([ff]), Rows).
 
 
-%problem(M,L):-
-%    length(M,L),
 
 blocks([], []).
 blocks([N1,N2|Ns1], [N3,N4|Ns2]) :-
         all_distinct([N1,N2,N3,N4]),
         blocks(Ns1, Ns2).
-
-
-
-goal(X,Z,Y):-
-      Y #= X+Z.
-%      Y is X + 2.
-%    abs(X-Y) #= 3.
 
 
 get_element([H|_],0, H).
